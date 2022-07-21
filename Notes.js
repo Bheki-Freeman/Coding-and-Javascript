0ed20951e24d1fad4ec37cbb4c12c3ee59718883
Bheki-Freeman <bhekisisadlamini05@gmail.com>
2022-07-21 12:15:56 +0200

    Added the table

--- Notes.js
+++ Notes.js
@@ -1,12 +1,12 @@
 /**
  * Creating a payroll programm, that will manage payments for employees at Freetechno enterprises
  * ******************************************
- * 
+ *
  * The program will capture employee name, DAte of employment, position, department and title
  * it will also take the total number of hours, payrate and tax
- * 
+ *
  * Then it will be able to take the gross pay, deductions and then display the net pay
- * 
+ *
  * ****************************************
- * 
+ *
  */
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const spn_date = document.querySelector("#spn_date")
-const date = new Date();
-
-//set date 
+//set date
 const setDate = () => {
-    spn_date.innerText = `${date.getDate()} - ${date.getMonth()+1} - ${date.getFullYear()}`
-}
+  const spn_date = document.querySelector("#spn_date");
+  const date = new Date();
+  spn_date.innerText = `${date.getDate()} - ${
+    date.getMonth() + 1
+  } - ${date.getFullYear()}`;
+};
 
 setDate();
